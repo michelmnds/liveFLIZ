2d2ed3117e26f748b7e2ee37d0f81278b35050c8
Michel Mendes <m1tchofc@gmail.com>
2025-02-28 12:54:56 +0000

    fix: remove w-80%

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import { NavBar } from './_partials/NavBar';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen w-[80%] w-full items-start justify-start gap-10">
+    <div className="flex min-h-screen w-full items-start justify-start gap-10">
       <NavBar />
       <main>{children}</main>
     </div>
